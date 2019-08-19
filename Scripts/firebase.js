6db6aac80bd7daa75c2f88bcb8aad26a976c5c1b
guayGonzalez <y.gonzalez.dev@lacortedelosbuhos.com>
2019-08-19 15:12:06 -0500

    form complete

---
+++ Scripts/firebase.js
@@ -0,0 +1,13 @@
+//initfirebase
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCVK--i2QJo_aZawkexPtkK6SD_IFLVRyI",
+  authDomain: "la-corte-de-lo-buhos.firebaseapp.com",
+  databaseURL: "https://la-corte-de-lo-buhos.firebaseio.com",
+  projectId: "la-corte-de-lo-buhos",
+  storageBucket: "",
+  messagingSenderId: "112259106430",
+  appId: "1:112259106430:web:79e9fec8a2deecf8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ Scripts/main.js
@@ -0,0 +1,44 @@
+
+//Reference contact collection
+
+var db =  firebase.firestore();
+
+//Get Contact info
+
+document.getElementById('mainForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+
+e.preventDefault();
+
+//input values
+
+var client = getInputValue('client');
+var email = getInputValue('email');
+var phone = getInputValue('phone');
+var product = getInputValue('product');
+
+sendContact(client, email, phone, product);
+
+}
+
+function getInputValue(id){
+
+return document.getElementById(id).value;
+
+}
+
+function sendContact(client, email, phone, product){
+
+    db.collection("Contact").add({
+        client: client,
+        email: email,
+        phone: phone,
+        product: product
+    }).then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    }).catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+
+}
